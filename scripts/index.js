794b8dfbf52da44329c24139521c787bc610cbcc
keanemind <keanemind@gmail.com>
2019-02-16 21:25:17 -0600

    Use promises for all API calls

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,10 @@ const base_uri = (
 // window.location.pathname includes leading slash?
 // window.location.hostname trailing slash?
 
+function isSuccess(statusCode) {
+    return Math.floor(statusCode / 100) === 2;
+}
+
 if (window.location.hash) {
     const hash = window.location.hash.substr(1);
     let result = {};
@@ -16,7 +20,7 @@ if (window.location.hash) {
 
     if (result['state'] && result['access_token']) {
         // Spotify just redirected here
-        performAPIRequests(result['state'].split(' '), result['access_token']);
+        performAPIRequests(result['access_token'], result['state'].split(' '));
     } else if (result['link']) {
         // performAPIRequsts() just redirected here
         displayResult(result['link']);
@@ -27,87 +31,163 @@ function displayResult(link) {
     alert("Here's your link: " + link)
 }
 
-function performAPIRequests(words, accessToken) {
-    let count = 0;
-    let tracks = {};
+function performAPIRequests(accessToken, words) {
+    searchSongs(accessToken, words).then(function(tracks) {
+        makePlaylist(accessToken).then(function(res) {
+            tracksEndpoint = res[0];
+            playlistURL = res[1];
+            addTracks(
+                accessToken, words, tracks, tracksEndpoint
+            ).then(function() {
+                // Redirect
+                window.location.replace(
+                    base_uri + '#link=' + encodeURIComponent(playlistURL)
+                );
+            }, function(error) {
+                // A song could not be added to the playlist
+                console.error(error);
 
-    let promise = new Promise(function(resolve, reject) {
-        for (let i = 0; i < words.length; i++) {
-            const request = new XMLHttpRequest();
-            request.onload = function() {
-                count++;
-                // Process the server response here.
-                const resp = JSON.parse(request.responseText);
-                for (let j = 0; j < resp['tracks']['items'].length; j++) {
-                    if (resp['tracks']['items'][j]['name'].toLowerCase() === words[i].toLowerCase()) {
-                        tracks[words[i]] = resp['tracks']['items'][j]['uri'];
-                        break;
-                    }
-                }
-                if (count === words.length) {
-                    resolve();
-                }
-            };
-
-            let txt = encodeURIComponent(words[i]);
-            request.open('GET', 'https://api.spotify.com/v1/search?q=' + txt + '&type=track', true);
-            request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-            request.send()
-        }
-    })
-    promise.then(function(){
-        makePlaylist(accessToken, words, tracks);
+                // Delete new playlist
+            });
+        }, function(error) {
+            // A new playlist could not be created
+            console.error(error);
+        });
+    }, function(error) {
+        // A song could not be found
+        console.error(error);
     });
 }
 
-function makePlaylist(accessToken, words, tracks) {
-    let track_uris_encoded = []; // song URIs in order of words in text
+function searchSongs(accessToken, words) {
+    const tracks = {};
+    const requestPromises = [];
     for (let i = 0; i < words.length; i++) {
-        if (tracks[words[i]]) {
-            track_uris_encoded.push(encodeURIComponent(tracks[words[i]]));
-        }
+        requestPromises.push(new Promise(function(resolve, reject) {
+            const request = new XMLHttpRequest();
+            request.onload = function() {
+                if (isSuccess(request.status)) {
+                    // Search for song in the response that matches word
+                    const resp = JSON.parse(request.responseText);
+                    const items = resp['tracks']['items'];
+                    for (let j = 0; j < items.length; j++) {
+                        if (
+                            items[j]['name'].toLowerCase() ===
+                            words[i].toLowerCase()
+                        ) {
+                            tracks[words[i]] = items[j]['uri'];
+                            resolve();
+                            return;
+                        }
+                    }
+                    reject(Error('Song not found.'));
+                } else {
+                    reject(Error(request.statusText));
+                }
+            };
+            request.onerror = function() {
+                reject(Error('Network Error'));
+            };
+            const txt = encodeURIComponent(words[i]);
+            request.open(
+                'GET',
+                'https://api.spotify.com/v1/search?q=' + txt + '&type=track'
+            );
+            request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+            request.send();
+        }));
     }
-    const track_uris_str = track_uris_encoded.join(',');
 
-    // Get user id
-    let id;
-    const requestGetId = new XMLHttpRequest();
-    requestGetId.onload = function() {
-        const resp = JSON.parse(requestGetId.responseText);
-        id = resp['id'];
-    }
-    requestGetId.open('GET', 'https://api.spotify.com/v1/me', false);
-    requestGetId.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-    requestGetId.send();
-
-    // Create playlist
-    let tracksEndpoint;
-    let newPlaylistURL;
-    const requestMakePlaylist = new XMLHttpRequest();
-    requestMakePlaylist.onload = function() {
-        const resp = JSON.parse(requestMakePlaylist.responseText);
-        tracksEndpoint = resp['tracks']['href'];
-        newPlaylistURL = resp['external_urls']['spotify'];
-    }
-    requestMakePlaylist.open('POST', 'https://api.spotify.com/v1/users/' + id + '/playlists', false);
-    requestMakePlaylist.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-    requestMakePlaylist.setRequestHeader('Content-Type', 'application/json');
-    requestMakePlaylist.send('{"name": "Diss Playlist"}');
-
-    // Add tracks
-    const requestAddTracks = new XMLHttpRequest();
-    requestAddTracks.onload = function() {
-        // Check if adding tracks was successful
-        // If failed, delete playlist
-        // If succeeded, redirect
-        window.location.replace(
-            base_uri + '#link=' + encodeURIComponent(newPlaylistURL));
-    }
-    requestAddTracks.open('POST', tracksEndpoint + '?uris=' + track_uris_str, false);
-    requestAddTracks.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-    requestAddTracks.send();
+    return new Promise(function(resolve, reject) {
+        Promise.all(requestPromises).then(function() {
+            resolve(tracks);
+        }, function(error) {
+            reject(error);
+        });
+    });
 }
 
+function makePlaylist(accessToken) {
+    return new Promise(function(resolve, reject) {
+        getUserId(accessToken).then(function(id) {
+            // Create playlist
+            const request = new XMLHttpRequest();
+            request.onload = function() {
+                if (isSuccess(request.status)) {
+                    const resp = JSON.parse(request.responseText);
+                    const tracksEndpoint = resp['tracks']['href'];
+                    const newPlaylistURL = resp['external_urls']['spotify'];
+                    resolve([tracksEndpoint, newPlaylistURL]);
+                } else {
+                    console.log('status: ' + request.status);
+                    reject(Error(request.statusText));
+                }
+            }
+            request.onerror = function() {
+                reject(Error('Network Error'));
+            };
+            request.open(
+                'POST',
+                'https://api.spotify.com/v1/users/' + id + '/playlists'
+            );
+            request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send('{"name": "Diss Playlist"}');
+        }, function(error) {
+            reject(error);
+        });
+    });
+}
+
+function addTracks(accessToken, words, tracks, tracksEndpoint) {
+    return new Promise(function(resolve, reject) {
+        let track_uris_encoded = []; // song URIs in order of words in text
+        for (let i = 0; i < words.length; i++) {
+            if (tracks[words[i]]) {
+                track_uris_encoded.push(encodeURIComponent(tracks[words[i]]));
+            }
+        }
+        const track_uris_str = track_uris_encoded.join(',');
+    
+        const request = new XMLHttpRequest();
+        request.onload = function() {
+            // Check if adding tracks was successful
+            // If failed, delete playlist
+            // If succeeded, redirect
+            if (isSuccess(request.status)) {
+                resolve();
+            } else {
+                reject(Error(request.statusText));
+            }
+        }
+        request.onerror = function() {
+            reject(Error('Network Error'));
+        }
+        request.open('POST', tracksEndpoint + '?uris=' + track_uris_str);
+        request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+        request.send();
+    });
+}
+
+function getUserId(accessToken) {
+    return new Promise(function(resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.onload = function() {
+            if (isSuccess(request.status)) {
+                const resp = JSON.parse(request.responseText);
+                resolve(resp['id']);
+            } else {
+                reject(Error(request.statusText));
+            }
+        }
+        request.onerror = function() {
+            reject(Error('Network Error'));
+        };
+        request.open('GET', 'https://api.spotify.com/v1/me');
+        request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+        request.send();
+    });
+}
 
 function redirect() {
     if (document.getElementById('text-field').value == '') {
